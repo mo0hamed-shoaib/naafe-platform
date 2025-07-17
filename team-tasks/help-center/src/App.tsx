24e2f9abbacce373ce5cb1eca0c38c059ac38d91
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-07-17 19:14:32 +0300

    complete refactor of repo

--- team-tasks/help-center/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HelpCenter from './components/HelpCenter';
-
-function App() {
-  return <HelpCenter activeSection="Verification" />;
-}
-
-export default App;
--- team-tasks/response-form/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import ServiceResponseForm from './components/ServiceResponseForm';
-import './App.css';
-
-function App() {
-  return <ServiceResponseForm />;
-}
-
-export default App;
