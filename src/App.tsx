72e725ab31430c7f6cf88ea21de92fbe1cfe35fc
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-08-25 11:03:34 +0300

    update App.tsx to include authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
+import { AuthProvider } from '@/contexts/AuthContext'
 import { Layout } from '@/components/layout/Layout'
+import { ProtectedRoute } from '@/components/auth/ProtectedRoute'
 import { HomePage } from '@/pages/HomePage'
+import { LoginPage } from '@/pages/auth/LoginPage'
+import { RegisterPage } from '@/pages/auth/RegisterPage'
+import { ForgotPasswordPage } from '@/pages/auth/ForgotPasswordPage'
+import { UserProfile } from '@/components/auth/UserProfile'
 import './styles/globals.css'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="naafe-ui-theme">
-      <Router>
-        <Layout>
+      <AuthProvider>
+        <Router>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* Add more routes here as we build them */}
+            {/* Public Routes */}
+            <Route path="/" element={<Layout><HomePage /></Layout>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            
+            {/* Protected Routes */}
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Layout>
+                  <UserProfile />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            
+            {/* Add more protected routes here as we build them */}
           </Routes>
-        </Layout>
-      </Router>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   )
 }
